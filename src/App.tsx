eb328331fb0e5c56716299972c129d97568b4511
이주형's Mac <leejuhyung@ijuhyeongsui-MacBookPro.local>
2022-03-04 09:53:12 +0900

    #2.11 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    // ES6문법
+    // ==const value = event.currentTarget.value;
+
+    // const value = event.currentTarget.value;
+    // const tagName = event.currentTarget.tagName;
+    // const width = event.currentTarget.width;
+    // const id = event.currentTarget.id;
+    // ==
+    // const{
+    //   cuttentTarget:{value,tagName,width,id}
+    // } = event;
+
+    // const x = event.x;
+    // const y = event.y;
+    // ==
+    // const { x, y } = event;
+
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
